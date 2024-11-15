59a502ba73ff9570435aa5e165e25a6e9cda6a84
Jean-Ely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-15 15:20:54 +0100

    Implement logic working in model Firestations and Firerstation

--- src/main/java/com/javabase/javatpsafetytnet/model/Firestation.java
+++ src/main/java/com/javabase/javatpsafetytnet/model/Firestation.java
@@ -1,4 +1,35 @@
 package com.javabase.javatpsafetytnet.model;
 
 public class Firestation {
+
+    private String address;
+    private String station;
+
+    public Firestation() {
+    }
+
+    public Firestation(String address, String station) {
+        this.address = address;
+        this.station = station;
+    }
+
+    // Getter
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    // Setter
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
 }
