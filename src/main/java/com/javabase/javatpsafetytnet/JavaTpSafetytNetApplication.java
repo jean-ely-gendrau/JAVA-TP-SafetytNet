01873263ae45c286a72799f5703af4e73baa3ab9
Jean-Ely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-15 14:55:25 +0100

    Init structur of project Safetynet

---
+++ src/main/java/com/javabase/javatpsafetytnet/JavaTpSafetytNetApplication.java
@@ -0,0 +1,13 @@
+package com.javabase.javatpsafetytnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTpSafetytNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTpSafetytNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javabase/javatpsafetytnet/JavaTpSafetytNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabase.javatpsafetytnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTpSafetytNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
