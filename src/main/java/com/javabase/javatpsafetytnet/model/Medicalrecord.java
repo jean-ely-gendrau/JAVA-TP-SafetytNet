855e440629fddf5d5d35ca4ee860c5a77ccf079f
Jean-Ely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-15 15:25:59 +0100

    Implement logic working in model Medicalrecords and Medicalrecord

--- src/main/java/com/javabase/javatpsafetytnet/model/Medicalrecord.java
+++ src/main/java/com/javabase/javatpsafetytnet/model/Medicalrecord.java
@@ -1,4 +1,53 @@
 package com.javabase.javatpsafetytnet.model;
 
+import java.util.List;
+
 public class Medicalrecord {
+
+    private String firstName;
+    private String lastname;
+    private String birthday;
+
+    List<String> medications;
+    List<String> allergies;
+
+    public Medicalrecord() {
+    }
+
+    public Medicalrecord(String firstName, String lastname, String birthday, List<String> medications, List<String> allergies) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    // Getter
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public List<String> getMedications() {
+        return medications;
+    }
+
+    public List<String> getAllergies() {
+        return allergies;
+    }
+
+    // Setter
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
