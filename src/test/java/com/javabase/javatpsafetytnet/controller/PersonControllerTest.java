b4da2f641233ae7ca8aa62b60bc904b357c614a1
jean-ely-gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-19 13:51:03 +0100

    Create Test Class PersonControllerTest with Generator Intellij

---
+++ src/test/java/com/javabase/javatpsafetytnet/controller/PersonControllerTest.java
@@ -0,0 +1,21 @@
+package com.javabase.javatpsafetytnet.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCommunityEmail() {
+    }
+
+    @Test
+    void getPersonInfo() {
+    }
+}
