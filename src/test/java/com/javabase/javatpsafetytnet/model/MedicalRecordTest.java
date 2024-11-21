aeacba37905578f40efc47c7c57df87cfe7a09f8
Jean-Ely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-21 09:43:23 +0100

    Implement Test testToString Class MedicalRecordTest

--- src/test/java/com/javabase/javatpsafetytnet/model/MedicalRecordTest.java
+++ src/test/java/com/javabase/javatpsafetytnet/model/MedicalRecordTest.java
@@ -27,12 +27,30 @@ class MedicalRecordTest {
 
     @BeforeEach
     void setUp(){
-        mockMedicationsList.clear();
-        mockAllergiesList.clear();
+        mockMedicalRecord.setLastName("Dupon");
+        mockMedicalRecord.setFirstName("Jean");
+        mockMedicalRecord.setBirthdate("12/31/2000");
+
+        mockMedicationsList.add("tramadol:2000MG");
+        mockMedicalRecord.setMedications(mockMedicationsList);
+
+        mockAllergiesList.add("pinut");
+        mockMedicalRecord.setAllergies(mockAllergiesList);
+
     }
 
     @Test
     void testToString() {
+
+        assertEquals("MedicalRecord{" +
+                "firstName='" + mockMedicalRecord.getFirstName() +
+                "', lastName='" + mockMedicalRecord.getLastName() +
+                "', birthdate='" + mockMedicalRecord.getBirthdate() +
+                "', medications='" + mockMedicalRecord.getMedications() +
+                "', allergies='" + mockMedicalRecord.getAllergies() +
+                "'}"
+                , mockMedicalRecord.toString());
+
     }
 
     @Test
@@ -44,51 +62,48 @@ class MedicalRecordTest {
 
     @Test
     void getLastName() {
-        mockMedicalRecord.setLastName("Dupon");
+      //  mockMedicalRecord.setLastName("Dupon");
 
         assertEquals("Dupon", mockMedicalRecord.getLastName());
     }
 
     @Test
     void getBirthdate() {
-        mockMedicalRecord.setBirthdate("12/31/2000");
+     //   mockMedicalRecord.setBirthdate("12/31/2000");
 
         assertEquals("12/31/2000", mockMedicalRecord.getBirthdate());
     }
 
     @Test
     void getMedications() {
-        mockMedicationsList.add("tramadol:2000MG");
-        mockMedicalRecord.setMedications(mockMedicationsList);
+
 
         assertEquals(mockMedicationsList, mockMedicalRecord.getMedications());
     }
 
     @Test
     void getAllergies() {
-        mockAllergiesList.add("pinut");
-        mockMedicalRecord.setAllergies(mockAllergiesList);
 
         assertEquals(mockAllergiesList, mockMedicalRecord.getAllergies());
     }
 
     @Test
     void setFirstName() {
-        mockMedicalRecord.setFirstName("Jean");
+      //  mockMedicalRecord.setFirstName("Jean");
 
         assertEquals("Jean", mockMedicalRecord.getFirstName());
     }
 
     @Test
     void setLastName() {
-        mockMedicalRecord.setLastName("Dupon");
+     //   mockMedicalRecord.setLastName("Dupon");
 
         assertEquals("Dupon", mockMedicalRecord.getLastName());
     }
 
     @Test
     void setBirthdate() {
-        mockMedicalRecord.setBirthdate("12/31/2000");
+       // mockMedicalRecord.setBirthdate("12/31/2000");
 
         assertEquals("12/31/2000", mockMedicalRecord.getBirthdate());
     }
