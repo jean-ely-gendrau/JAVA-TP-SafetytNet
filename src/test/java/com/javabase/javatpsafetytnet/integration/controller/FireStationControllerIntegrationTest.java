8c03960bac3863743e9097b250f69e7322cda6d4
Jean-Ely Gendrau <jean-ely.gendrau@laplateforme.io>
2024-11-21 22:26:42 +0100

    Refactor Test in FireStationControllerIntegrationTest fireAlert() and implement floodStation()

--- src/test/java/com/javabase/javatpsafetytnet/integration/controller/FireStationControllerIntegrationTest.java
+++ src/test/java/com/javabase/javatpsafetytnet/integration/controller/FireStationControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import com.javabase.javatpsafetytnet.repository.PersonRepository;
 import com.javabase.javatpsafetytnet.service.FireStationService;
 import com.javabase.javatpsafetytnet.service.MedicalRecordService;
 import com.javabase.javatpsafetytnet.service.PersonService;
+import com.javabase.javatpsafetytnet.service.dto.PersonContactMedicalHistoryDTO;
 import com.javabase.javatpsafetytnet.service.dto.PersonsByStationDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,14 +55,30 @@ class FireStationControllerIntegrationTest {
 
     @Test
     void fireAlert() {
-        List<String> listFirestationNumber = new ArrayList<>();
-        listFirestationNumber.add(0,"1");
+        String address = "892 Downing Ct";
 
-        List<PersonsByStationDTO> personsByStationDTOList = fireStationService.getAllPersonsByStations(listFirestationNumber);
+        PersonsByStationDTO personsByStationDTOList = fireStationService.getAllPersonsByAddress(address);
+
+        Integer countListPerson = personsByStationDTOList.getPersonFireAlertList().size();
+
+        assertNotNull(personsByStationDTOList);
+        assertEquals("2", personsByStationDTOList.getFirestation());
+        assertEquals(address, personsByStationDTOList.getAddress());
+        assertEquals("Zemicks", personsByStationDTOList.getPersonFireAlertList().get(0).getLastName());
+        assertEquals("7", personsByStationDTOList.getPersonFireAlertList().get(countListPerson - 1).getAge());
     }
 
     @Test
     void floodStation() {
+        List<String> listFirestationNumber = new ArrayList<>();
+        listFirestationNumber.add(0,"1");
+        listFirestationNumber.add(1,"2");
+
+        List<PersonsByStationDTO> personsByStationDTOList = fireStationService.getAllPersonsByStations(listFirestationNumber);
+
+        assertNotNull(personsByStationDTOList);
+        assertEquals( "2",personsByStationDTOList.get(0).getFirestation());
+        assertEquals("29 15th St", personsByStationDTOList.get(0).getAddress());
     }
 
     @Test
